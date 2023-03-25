b67b5af044a2bcd2deab744bfd6aed345e2886ce
pdurasie <pdurasie@pm.me>
2023-03-25 16:10:41 +0100

    Modify python script to create a new table in my database

--- container-python-scripts/database_scripts/create_sub_sections.py
+++ container-python-scripts/database_scripts/create_sub_sections.py
@@ -89,63 +89,120 @@ SELECT \"osm_id\",\"access\",\"amenity\",\"area\",\"barrier\",\"bicycle\",\"bran
 FROM pois_query
 WHERE ST_Contains(ST_MakeEnvelope({min_lon}, {min_lat}, {max_lon}, {max_lat}, 4326), \"way\")
    OR ST_Intersects(ST_MakeEnvelope({min_lon}, {min_lat}, {max_lon}, {max_lat}, 4326), \"way\");""".format(
-        min_lon=xmin, min_lat=ymin, max_lon=xmax, max_lat=ymax)
+        min_lon=xmin, min_lat=ymin, max_lon=xmax, max_lat=ymax
+    )
 
 
 # Connect to the database
-conn = psycopg2.connect(database="docker", user="docker",
-                        password="docker", host="postgis_db", port="5432")
+conn = psycopg2.connect(
+    database="docker", user="docker", password="docker", host="postgis_db", port="5432"
+)
 cur = conn.cursor()
 
-geo_hashes = ["u33677", "u33dh4", "u33e5h"]
+geo_hashes = []
 
-# with open('../berlin_data/geohashes_berlin_7.csv', newline='') as csvfile:
-#     objects = csv.reader(csvfile, delimiter=',')
-#     for object in objects:
-#         geo_hashes.append(object)
+with open("../berlin_data/geohashes_berlin_7.csv", newline="") as csvfile:
+    objects = csv.reader(csvfile, delimiter=",")
+    for object in objects:
+        geo_hashes.append(object)
 
-# cur.execute("CREATE TABLE berlin_regions(SERIAL PRIMARY KEY)")
+# If berlin_regions table does not exist, create it
+# If it exists, drop it and create it again
+cur.execute("DROP TABLE IF EXISTS berlin_regions")
+cur.execute(
+    """
+CREATE TABLE berlin_regions(
+    id SERIAL PRIMARY KEY,
+    osm_id bigint,
+    geohash text,
+    access text,
+    amenity text,
+    area text,
+    barrier text,
+    bicycle text,
+    brand text,
+    bridge text,
+    boundary text,
+    building text,
+    culvert text,
+    embankment text,
+    foot text,
+    harbour text,
+    highway text,
+    landuse text,
+    leisure text,
+    lock text,
+    name text,
+    "natural" text,
+    place text,
+    surface text,
+    tourism text,
+    tracktype text,
+    water text,
+    waterway text,
+    wetland text,
+    wood text,
+    tags text,
+    area_fraction float,
+    way geometry
+)
+"""
+)
+
 
 for geohash in geo_hashes:
     # Retrieve the polygons from the database that fit the given geohash
     # also save some logs when there is a geohash that has no region in it
     xmin, ymin, xmax, ymax = bbox(geohash)
     # cur.execute(get_SQL_command(xmin, ymin, xmax, ymax))
-    cur.execute("SELECT \"osm_id\",\"access\",\"amenity\",\"area\",\"barrier\",\"bicycle\",\"brand\",\"bridge\",\"boundary\",\"building\",\"culvert\",\"embankment\",\"foot\",\"harbour\",\"highway\",\"landuse\",\"leisure\",\"lock\",\"name\",\"natural\",\"place\",\"surface\",\"tourism\", \"tracktype\",\"water\",\"waterway\",\"wetland\",\"wood\",\"tags\", ST_AsText(\"way\") AS \"way\" FROM public.planet_osm_polygon LIMIT 10")
+    cur.execute(
+        f"""
+    WITH geohash_bbox AS (
+  SELECT ST_Transform(ST_SetSRID(ST_GeomFromGeoHash('{geohash}'), 4326), 3857) AS geom
+)
+SELECT
+\"osm_id\",\"access\",\"amenity\",\"area\",\"barrier\",\"bicycle\",\"brand\",\"bridge\",\"boundary\",\"building\",\"culvert\",\"embankment\",\"foot\",\"harbour\",\"highway\",\"landuse\",\"leisure\",\"lock\",\"name\",\"natural\",\"place\",\"surface\",\"tourism\", \"tracktype\",\"water\",\"waterway\",\"wetland\",\"wood\",\"tags\",ST_AsText(ST_Transform(planet_osm_polygon.way, 4326)) AS \"way\"
+FROM
+  planet_osm_polygon,
+  geohash_bbox
+WHERE
+  ST_Intersects(planet_osm_polygon.way, geohash_bbox.geom);
+
+    """
+    )
     rows = cur.fetchall()
 
     # Process the polygons
     for row in rows:
-        print(str(row))
-        # osm_id = row[0]
-        # access = row[1]
-        # amenity = row[2]
-        # area = row[3]
-        # barrier = row[4]
-        # bicycle = row[5]
-        # brand = row[6]
-        # bridge = row[7]
-        # boundary = row[8]
-        # buildings = row[9]
-        # culvert = row[10]
-        # embankment = row[11]
-        # foot = row[12]
-        # harbour = row[13]
-        # highway = row[14]
-        # landuse = row[15]
-        # leisure = row[16]
-        # lock = row[17]
-        # name = row[18]
-        # natural = row[19]
-        # place = row[20]
-        # surface = row[21]
-        # tourism = row[22]
-        # tracktype = row[23]
-        # watermark = row[24]
-        # waterway = row[25]
-        # wetland = row[26]
-        # wood = row[27]
-        # tags = row[28]
+        osm_id = row[0]
+        access = row[1]
+        amenity = row[2]
+        area = row[3]
+        barrier = row[4]
+        bicycle = row[5]
+        brand = row[6]
+        bridge = row[7]
+        boundary = row[8]
+        buildings = row[9]
+        culvert = row[10]
+        embankment = row[11]
+        foot = row[12]
+        harbour = row[13]
+        highway = row[14]
+        landuse = row[15]
+        leisure = row[16]
+        lock = row[17]
+        name = row[18]
+        natural = row[19]
+        place = row[20]
+        surface = row[21]
+        tourism = row[22]
+        tracktype = row[23]
+        watermark = row[24]
+        waterway = row[25]
+        wetland = row[26]
+        wood = row[27]
+        tags = row[28]
         geometry = wkt.loads(row[29])
 
         intersecting_geometry = geometry
@@ -155,11 +212,66 @@ for geohash in geo_hashes:
         elif isinstance(intersecting_geometry, Polygon):
             intersecting_geometry = get_intersecting_polygon(geohash, geometry)
 
-        # # Insert the sub polygons into the new table
-        #     geometry_wkt = intersecting_geometry.wkt
-        # TODO add more columns to this than id and polygon, also the area it covers!
-        # cur.execute(
-        # "INSERT INTO berlin_regions (\"osm_id\",\"access\",\"amenity\",\"area\",\"barrier\",\"bicycle\",\"brand\",\"bridge\",\"boundary\",\"building\",\"culvert\",\"embankment\",\"foot\",\"harbour\",\"highway\",\"landuse\",\"leisure\",\"lock\",\"name\",\"natural\",\"place\",\"surface\",\"tourism\", NULL AS \"tracktype\",\"water\",\"waterway\",\"wetland\",\"wood\",\"tags\",\"way\") VALUES (%s, ST_GeomFromText(%s));", (parent_id, geometry_wkt))
+        # Create a new row which holds all the information of the original row, only replacing the geometry with the intersecting geometry if that is non null, otherwise the original geometry. Also add the geohash to the row and the fraction of the area the geometry covers of the geohash.
+        to_be_inserted_geometry = (
+            intersecting_geometry if intersecting_geometry is not None else geometry
+        )
+
+        new_row = (
+            osm_id,
+            geohash,
+            access,
+            amenity,
+            area,
+            barrier,
+            bicycle,
+            brand,
+            bridge,
+            boundary,
+            buildings,
+            culvert,
+            embankment,
+            foot,
+            harbour,
+            highway,
+            landuse,
+            leisure,
+            lock,
+            name,
+            natural,
+            place,
+            surface,
+            tourism,
+            tracktype,
+            watermark,
+            waterway,
+            wetland,
+            wood,
+            tags,
+            to_be_inserted_geometry.area / geometry.area,
+            to_be_inserted_geometry.wkt,
+        )
+
+        # Insert the new row into the database
+        cur.execute(
+            """
+            INSERT INTO berlin_regions (
+                osm_id, geohash, access, amenity, area, barrier, bicycle, brand, bridge, boundary,
+                building, culvert, embankment, foot, harbour, highway, landuse, leisure, lock,
+                name, "natural", place, surface, tourism, tracktype, water, waterway, wetland,
+                wood, tags, area_fraction, way
+            ) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s);
+            """,
+            new_row,
+        )
+
+    # Print out the number of rows that were inserted and the original number of rows
+    cur.execute("SELECT COUNT(*) FROM berlin_regions")
+    print("Number of rows in berlin_regions: ", cur.fetchone()[0])
+    print("\n")
+    print("Number of rows in original table: ", len(rows))
+    # cur.execute(
+    # "INSERT INTO berlin_regions (\"osm_id\",\"access\",\"amenity\",\"area\",\"barrier\",\"bicycle\",\"brand\",\"bridge\",\"boundary\",\"building\",\"culvert\",\"embankment\",\"foot\",\"harbour\",\"highway\",\"landuse\",\"leisure\",\"lock\",\"name\",\"natural\",\"place\",\"surface\",\"tourism\", NULL AS \"tracktype\",\"water\",\"waterway\",\"wetland\",\"wood\",\"tags\",\"way\") VALUES (%s, ST_GeomFromText(%s));", (parent_id, geometry_wkt))
 
 # Commit the changes and close the connection
 conn.commit()
--- container-python-scripts/database_scripts/wkt_geometry_util.py
+++ container-python-scripts/database_scripts/wkt_geometry_util.py
@@ -1,34 +1,46 @@
 from geohash import bbox
 from shapely.geometry import Polygon
+from shapely.geometry import MultiPolygon
 from shapely.geometry import LineString
+import shapely.wkt as wkt
 
 
-def get_intersecting_polygon(geohash: str, polygon: Polygon) -> Polygon | None:
+def get_intersecting_polygon(
+    geohash: str, polygon: Polygon
+) -> Polygon | MultiPolygon | None:
     # Convert geohash to bounding box polygon
     # Example value of bbox(geohash): {'s': 52.4432373046875, 'w': 13.150634765625, 'n': 52.44873046875, 'e': 13.16162109375}
     # This means that the southern boundary of the bounding box is at latitude 52.4432373046875, the western boundary is at longitude 13.150634765625, the northern boundary is at latitude 52.44873046875, and the eastern boundary is at longitude 13.16162109375.
     # The lines that the lat/long boundaries draw intersect, forming the geohash bounding box.
     min_lat, min_lon, max_lat, max_lon = bbox(geohash).values()
     bbox_polygon = Polygon(
-        [(min_lon, min_lat), (max_lon, min_lat), (max_lon, max_lat), (min_lon, max_lat)])
+        [(min_lon, min_lat), (max_lon, min_lat), (max_lon, max_lat), (min_lon, max_lat)]
+    )
 
     # Check for intersection and get intersecting part as a new polygon
     if polygon.intersects(bbox_polygon):
-        return polygon.intersection(bbox_polygon)
+        intersection = polygon.intersection(bbox_polygon)
+        if not (intersection.equals_exact(polygon, 0)):
+            return intersection
 
 
 def get_intersecting_line(geohash: str, line: LineString) -> LineString | None:
     # Convert geohash to bounding box polygon
     min_lat, min_lon, max_lat, max_lon = bbox(geohash)
     bbox_polygon = Polygon(
-        [(min_lon, min_lat), (max_lon, min_lat), (max_lon, max_lat), (min_lon, max_lat)])
+        [(min_lon, min_lat), (max_lon, min_lat), (max_lon, max_lat), (min_lon, max_lat)]
+    )
 
     # Get intersection between the line and the bounding box polygon
     intersection = line.intersection(bbox_polygon)
     if intersection.is_empty:
         return None
-    elif intersection.geom_type == 'LineString':
+    elif intersection.geom_type == "LineString":
         return intersection
     else:
         # If the intersection is a MultiLineString, convert it to a LineString by merging all lines together
-        return intersection[0].merged if intersection.geom_type == 'MultiLineString' else intersection
+        return (
+            intersection[0].merged
+            if intersection.geom_type == "MultiLineString"
+            else intersection
+        )
